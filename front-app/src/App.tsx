6f76aa7ae0a097142d3794d410a5f586a9fc3d47
Onora <onora.hubleur@pm.me>
2025-01-01 16:25:22 +0100

    front change

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         count != null && (
           <div className="card">
             <button onClick={onClick}>
-              Counter: {count}
+              Counter : {count}
             </button>
           </div>
         )
